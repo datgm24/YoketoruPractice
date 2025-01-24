b14d564ca2a5b2210aba69d8b114810a315bc045
YuTanaka <tanakaedu@users.noreply.github.com>
2025-01-24 16:01:34 +0900

    入力ベンチ

---
+++ Assets/Yoketoru/Scripts/Game/Player/InputController.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class InputController
+{
+    IInput[] inputs =
+    {
+
+    };
+
+    public void Update()
+    {
+
+    }
+
+    public Vector2 GetValue()
+    {
+        return Vector2.zero;
+    }
+}
